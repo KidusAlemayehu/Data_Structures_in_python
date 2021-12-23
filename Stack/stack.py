9e4986381cc4042afd1d353b5c9e6be80a3d4066
KidusAlemayehu <kidusalemayehu705@gmail.com>
2021-12-23 16:44:50 +0300

    Stack

--- Stack/stack.py
+++ Stack/stack.py
@@ -1,3 +1,5 @@
+import sys
+
 class Stack:
     def __init__(self, limit):
         self.items = []
@@ -5,10 +7,14 @@ class Stack:
         self.limit = limit
         
     def push(self, item):
+        if self.isFull():
+            return f'Stack is Full'
         self.peek += 1
         return self.items.append(item)
     
     def pop(self):
+        if self.isEmpty():
+            return f'Stack is already empty'
         self.peek += 1
         return self.items.pop()
 
@@ -25,3 +31,39 @@ class Stack:
     def display(self):
         for item in self.items:
             print(item)
+
+    def num_of_items(self):
+        return len(self.items)
+
+
+def main():
+    global stack
+    print("Stack")
+    print("**********************")
+    print("1. create")
+    print("2. push")
+    print("3. pop")
+    print("4. display")
+    print("5. Exit")
+    while True:
+        ch = int(input("enter your choice: "))
+        if ch == 1:
+            limit = int(input("enter your limit: "))
+            stack = Stack(limit)
+            print('created successfully')
+        elif ch == 2:
+            item = input("enter the item: ")
+            stack.push(item)
+            print('{} added successfully' .format(item))
+        elif ch == 3:
+            stack.pop()
+            print('removed successfully')
+        elif ch == 4:
+            print(" items in the stack")
+            stack.display()
+        elif ch == 5:
+            sys.exit()
+
+
+if __name__ == '__main__':
+    main()
